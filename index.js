36765a8a34450148dab7117e8cf674d19697a512
kelly r maass <kmaass77627@gmail.com>
2020-09-17 06:50:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ if (left > 0) {
 }
 
 function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
   if (left < 100) {
     dodger.style.left = `${left - 1}px`;
   }

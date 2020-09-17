f7d168c488bf439ee5725077896136647877235b
kelly r maass <kmaass77627@gmail.com>
2020-09-17 06:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+let dodger = document.getElementById(`dodger`);
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+    
+  }
+  
+  
+
+
+function moveDodgerLeft() {
+  
+}
+
+function moveDodgerRight() {
+  
+}
